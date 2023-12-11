743c1654ccb63019af2330eaeb48be3fb1cce62c
Jorrit Leysen <91262430+JorritLeysen@users.noreply.github.com>
2023-12-11 21:51:58 +0100

    Update DLTask.py

--- MLTask.py
+++ MLTask.py
@@ -1,78 +1,170 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from ucimlrepo import fetch_ucirepo
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import HistGradientBoostingClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-import category_encoders as ce
 import streamlit as st
+import re
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import os
+import requests
+from selenium.webdriver.common.by import By
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing import image_dataset_from_directory
 
-# Fetch dataset
-car_evaluation = fetch_ucirepo(id=19)
+NUM_CLASSES = 6
+IMG_SIZE = 64
+HEIGTH_FACTOR = 0.2
+WIDTH_FACTOR = 0.2
 
-# Data (as pandas dataframes)
-car_evaluation_df = pd.DataFrame(car_evaluation.data.original)
-feature_cols = ['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety']
+def fetch_image_urls(query: str, max_links_to_fetch: int, wd: webdriver, sleep_between_interactions: int = 1):
 
-X = car_evaluation_df[feature_cols]  # Features
-y = car_evaluation_df['class']  # Labels
+    def scroll_to_end(wd):
+        wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(sleep_between_interactions)
+    
+    # Build the Google Images URL
+    search_url = "https://www.google.com/search?q={}&tbm=isch".format(query)
+    wd.get(search_url)
 
-ce_ord = ce.OrdinalEncoder(cols=feature_cols)
-X_cat = ce_ord.fit_transform(X)
+    id_value = "yDmH0d"
+    xpath_expression = f"//*[@id='{id_value}']/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/div[1]/form[1]/div/div/button"
+    button_element = wd.find_element(By.XPATH, xpath_expression)
 
-ce_ordY = ce.OrdinalEncoder(cols='class')
-Y_cat = ce_ordY.fit_transform(y)
+    # Click the button
+    button_element.click()
 
-# Split dataset into training set and test set
-X_train, X_test, y_train, y_test = train_test_split(X_cat, Y_cat, test_size=0.3, random_state=42)  # 70% training and 30% test
+    image_urls = set()
+    image_count = 0
 
-# Display the accuracy for Decision Tree
-clf = DecisionTreeClassifier(criterion="entropy")
-clf = clf.fit(X_train, y_train)
-y_pred_dt = clf.predict(X_test)
-accuracy_dt = accuracy_score(y_test, y_pred_dt)
+    while image_count < max_links_to_fetch:
+        scroll_to_end(wd)
+        time.sleep(sleep_between_interactions)
 
-# Display the accuracy for K-Nearest Neighbors
-clf_knn = KNeighborsClassifier()
-clf_knn.fit(X_train, y_train)
-y_pred_knn = clf_knn.predict(X_test)
-accuracy_knn = accuracy_score(y_test, y_pred_knn)
+        # Extract image URLs directly from the page source using regular expressions
+        page_source = wd.page_source
+        matches = re.findall(r'img\ssrc=\"(https:[^"]+)', page_source)
+        image_urls.update(matches)
 
-# Display the accuracy for HistGradientBoostingClassifier
-clf_hgbc = HistGradientBoostingClassifier(max_iter=100)
-clf_hgbc.fit(X_train, y_train)
-y_pred_hgbc = clf_hgbc.predict(X_test)
-accuracy_hgbc = accuracy_score(y_test, y_pred_hgbc)
+        image_count = len(image_urls)
 
-# Streamlit app
-st.title('Car Evaluation Dataset')
+        if image_count >= max_links_to_fetch:
+            print(f"Found {image_count} image links")
+            break
+        else:
+            print(f"Found {image_count} image links, looking for more ...")
+            time.sleep(sleep_between_interactions)  # Adjust as needed
 
-st.subheader('Dataset Information')
-st.write(f"Dataset Size: {car_evaluation_df.shape[0]} rows and {car_evaluation_df.shape[1]} columns")
-st.write('Features:', feature_cols)
-st.write('Target:', 'class')
+    return image_urls
 
-# Show the target (class) distribution
-st.write("Target Distribution:")
-st.write(y.value_counts())
+def download_images(folder_path: str, file_prefix: str, image_urls: set):
+    for i, url in enumerate(image_urls):
+        try:
+            response = requests.get(url)
+            # Ensure that the image is valid
+            if response.status_code == 200:
+                with open(os.path.join(folder_path, f"{file_prefix}_{i}.jpg"), 'wb') as file:
+                    file.write(response.content)
+        except Exception as e:
+            print(f"Error downloading image {i + 1}: {e}")
 
-# Display the first few rows of the dataset
-st.write("Sample Data:")
-st.write(X.head())
-st.write(y.head())
+def create_model():
+    model = tf.keras.Sequential([
+        layers.Resizing(IMG_SIZE, IMG_SIZE),
+        layers.Rescaling(1./255),
+        layers.RandomFlip("horizontal"),
+        layers.RandomTranslation(HEIGTH_FACTOR, WIDTH_FACTOR),
+        layers.RandomZoom(0.2),
+        layers.Conv2D(32, (3, 3), input_shape=(IMG_SIZE, IMG_SIZE, 3), activation="relu"),
+        layers.MaxPooling2D((2, 2)),
+        layers.Dropout(0.2),
+        layers.Conv2D(32, (3, 3), activation="relu"),
+        layers.MaxPooling2D((2, 2)),
+        layers.Dropout(0.2),
+        layers.Flatten(),
+        layers.Dense(128, activation="relu"),
+        layers.Dense(NUM_CLASSES, activation="softmax")  # Use softmax for multi-class classification
+    ])
+    model.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
 
-st.subheader('Decision Tree Classifier')
-st.write(f"Accuracy: {accuracy_dt:.2f}")
+def main():
+    st.title("Google Images Scraper & Classifier with Streamlit")
 
-st.subheader('K-Nearest Neighbors Classifier')
-st.write(f"Accuracy: {accuracy_knn:.2f}")
+    # Sidebar for Scraping
+    st.sidebar.header("Image Scraping Settings")
+    query = st.sidebar.text_input("Enter search query:")
+    max_links_to_fetch = st.sidebar.number_input("Max links to fetch:", value=150)
 
-st.subheader('HistGradientBoosting Classifier')
-st.write(f"Accuracy: {accuracy_hgbc:.2f}")
+    if st.sidebar.button("Scrape Images"):
+        # Execute the scraping and downloading
+        st.sidebar.text("Scraping in progress...")
 
-# Add more Streamlit components for visualizing EDA and other comparisons
+        # Disable webdriver logs to avoid cluttering the Streamlit interface
+        webdriver_options = webdriver.ChromeOptions()
+        webdriver_options.add_argument("--headless")
+        webdriver_options.add_argument("--disable-gpu")
+        webdriver_options.add_argument("--log-level=3")
+        wd = webdriver.Chrome(options=webdriver_options)
 
-# Display confusion matrices or graphs here
+        try:
+            image_urls = fetch_image_urls(query, max_links_to_fetch)
+            folder_path = f"./images/{query.lower()}"
+            os.makedirs(folder_path, exist_ok=True)
+            download_images(folder_path, query.lower(), image_urls)
+            st.sidebar.text(f"Scraping complete. Images saved in {folder_path}")
+        except Exception as e:
+            st.sidebar.text(f"Error: {e}")
+        finally:
+            wd.quit()
+
+    # Sidebar for Model Training
+    st.sidebar.header("Model Training Settings")
+
+    # Create the training dataset from the 'images' directory
+    train_ds = image_dataset_from_directory(
+        directory='./images',
+        labels='inferred',
+        label_mode='categorical',
+        batch_size=16,
+        image_size=(IMG_SIZE, IMG_SIZE),
+        validation_split=0.2,
+        subset='training',
+        seed=123
+    )
+
+    # Create the validation dataset from the 'images' directory
+    validation_ds = image_dataset_from_directory(
+        directory='./images',
+        labels='inferred',
+        label_mode='categorical',
+        batch_size=16,
+        image_size=(IMG_SIZE, IMG_SIZE),
+        validation_split=0.2,
+        subset='validation',
+        seed=123
+    )
+
+    # Create the testing dataset from the 'testimages' directory
+    test_ds = image_dataset_from_directory(
+        directory='./testimages',
+        labels='inferred',
+        label_mode='categorical',
+        batch_size=16,
+        image_size=(IMG_SIZE, IMG_SIZE)
+    )
+
+    # Train the model
+    model = create_model()
+    history = model.fit(train_ds, validation_data=validation_ds, epochs=20)
+
+    # Display the training history
+    st.subheader("Training History")
+    st.line_chart(history.history)
+
+    # Evaluate the model on the test dataset
+    test_loss, test_acc = model.evaluate(test_ds)
+    st.subheader("Test Accuracy")
+    st.write(f'Test accuracy: {test_acc:.4f}')
+
+if __name__ == "__main__":
+    main()
